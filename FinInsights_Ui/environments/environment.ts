8b55e7b9e6caeeec1d91d5a3ccb4dbc359c76514
Raghunandan V <raghunandanv@Raghunandans-MacBook-Air.local>
2026-06-29 01:10:42 +0530

    Use Render API endpoint

--- FinInsights_Ui/environments/environment.ts
+++ FinInsights_Ui/environments/environment.ts
@@ -1,5 +1,10 @@
 export const environment = {
   production: false,
+<<<<<<< HEAD
   apiUrl: 'https://fininsights-ny3t.onrender.com'
 };
 
+=======
+  apiUrl: 'http://localhost:5000'
+};
+>>>>>>> f2ff07c (Use Render API endpoint)
