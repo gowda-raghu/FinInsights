1427efe09ea17bdc7ae81263cbae57722dbeffb5
Raghunandan V <raghunandanv@Raghunandans-MacBook-Air.local>
2026-06-29 10:03:34 +0530

    Polly

--- FinInsights_API/Test/BusinessLayer/Services/ServiceInvoker.cs
+++ FinInsights_API/Test/BusinessLayer/Services/ServiceInvoker.cs
@@ -1,3 +1,6 @@
+using Polly;
+using Polly.Retry;
+
 public class ServiceInvoker : IServiceInvoker
 {
     private readonly IHttpClientFactory _clientFactory;
@@ -7,36 +10,71 @@ public class ServiceInvoker : IServiceInvoker
         _clientFactory = clientFactory;
     }
 
-    public async Task<TResponse?> PostAsync<TRequest, TResponse>(
+    public async Task<TResponse> PostAsync<TRequest, TResponse>(
     string url,
     TRequest request)
-{
-    var client = _clientFactory.CreateClient("InternalServices");
-
-    var response = await client.PostAsJsonAsync(url, request);
-
-    var responseContent = await response.Content.ReadAsStringAsync();
-
-    if (!response.IsSuccessStatusCode)
     {
-        throw new Exception(
-            $"URL: {url}\n" +
-            $"Status Code: {(int)response.StatusCode} ({response.StatusCode})\n" +
-            $"Response: {responseContent}");
-    }
+        AsyncRetryPolicy<HttpResponseMessage> retryPolicy =
+            Policy<HttpResponseMessage>
+                .Handle<HttpRequestException>()
+                .OrResult(r =>
+                    r.StatusCode == System.Net.HttpStatusCode.BadGateway ||
+                    r.StatusCode == System.Net.HttpStatusCode.ServiceUnavailable ||
+                    r.StatusCode == System.Net.HttpStatusCode.GatewayTimeout)
+                .WaitAndRetryAsync(
+                    retryCount: 3,
+                    sleepDurationProvider: retryAttempt =>
+                        TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                );
 
-    if (string.IsNullOrWhiteSpace(responseContent))
-    {
-        return default;
-    }
+        var _httpClient = _clientFactory.CreateClient("InternalServices");
 
-    return System.Text.Json.JsonSerializer.Deserialize<TResponse>(
-        responseContent,
-        new System.Text.Json.JsonSerializerOptions
+        var response = await retryPolicy.ExecuteAsync(() =>
+            _httpClient.PostAsJsonAsync(url, request));
+
+        if (!response.IsSuccessStatusCode)
         {
-            PropertyNameCaseInsensitive = true
-        });
-}
+            var error = await response.Content.ReadAsStringAsync();
+
+            throw new Exception(
+                $"URL: {url}\n" +
+                $"Status Code: {(int)response.StatusCode} ({response.StatusCode})\n" +
+                $"Response: {error}");
+        }
+
+        return await response.Content.ReadFromJsonAsync<TResponse>();
+    }
+
+    //     public async Task<TResponse?> PostAsync<TRequest, TResponse>(
+    //     string url,
+    //     TRequest request)
+    // {
+    //     var client = _clientFactory.CreateClient("InternalServices");
+
+    //     var response = await client.PostAsJsonAsync(url, request);
+
+    //     var responseContent = await response.Content.ReadAsStringAsync();
+
+    //     if (!response.IsSuccessStatusCode)
+    //     {
+    //         throw new Exception(
+    //             $"URL: {url}\n" +
+    //             $"Status Code: {(int)response.StatusCode} ({response.StatusCode})\n" +
+    //             $"Response: {responseContent}");
+    //     }
+
+    //     if (string.IsNullOrWhiteSpace(responseContent))
+    //     {
+    //         return default;
+    //     }
+
+    //     return System.Text.Json.JsonSerializer.Deserialize<TResponse>(
+    //         responseContent,
+    //         new System.Text.Json.JsonSerializerOptions
+    //         {
+    //             PropertyNameCaseInsensitive = true
+    //         });
+    // }
 
     public async Task<TResponse?> GetAsync<TResponse>(
         string url)
